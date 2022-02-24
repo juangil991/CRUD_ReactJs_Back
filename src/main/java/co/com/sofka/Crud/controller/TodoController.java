2a7585d3f852ac5b1e741674ca8cbbcf43af2f1b
juangil991 <jcgo125@gmail.com>
2022-02-23 19:38:08 -0500

    se el put para que devuelva el dato que se actualizo para integracion con el front

--- src/main/java/co/com/sofka/Crud/controller/TodoController.java
+++ src/main/java/co/com/sofka/Crud/controller/TodoController.java
@@ -7,23 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation. *;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TodoController {
 
     @Autowired
     TodoService todoService;
 
-    @GetMapping(value="api/list")
+    @GetMapping(value="api/todo")
     public Iterable<Todo> list(){
         return todoService.list();
     }
     @PostMapping(value="api/todo")
-    public void save(@RequestBody Todo todo){
-        todoService.save(todo);
+    public Todo save(@RequestBody Todo todo){
+       return todoService.save(todo);
     }
     @PutMapping(value="api/todo")
-    public void update(@RequestBody Todo todo){
-        if(todo.getId()!=null) todoService.save(todo);
-        throw new RuntimeException("no existe id para actualizar");
+    public Todo update(@RequestBody Todo todo){
+        if(todo.getId()!=null) return todoService.save(todo);
+      else  throw new RuntimeException("no existe id para actualizar");
     }
     @DeleteMapping(value = "api/{id}/todo")
     public void delete(@PathVariable("id")Long id){
